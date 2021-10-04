152df83bc07031912fb060d4498cc0e69f944678
unknown <khomanesuraj8@gmail.com>
2021-10-04 19:05:25 +0530

    create board

--- src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
+++ src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
@@ -1,7 +1,16 @@
 package com.Bridgelabz.TicTacToe;
 
 public class TicTacToeGame {
+	public static char[] board;
 	public static void main(String[] args){
         System.out.println("play game");
+        createBoard();
    }
+	//creating method createBoard
+		public static void createBoard(){
+			board = new char[10];
+	        	for(int i = 1; i < board.length; i++){
+	                	board[i] = ' ';
+	        	}
+	     }
 }
