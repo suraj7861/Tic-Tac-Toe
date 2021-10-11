b916686b9754194f1806ad0c3a02ae1de17952f2
unknown <khomanesuraj8@gmail.com>
2021-10-11 12:21:58 +0530

    Make code indentation right

--- src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
+++ src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
@@ -44,7 +44,7 @@ public class TicTacToeGame {
 		//print empty board 
 		System.out.println(board[1] + " |" + board[2] + " |" + board[3]);
 		System.out.println("--------");
-        System.out.println(board[4] + " |" + board[5] + " |" + board[6]);
+		System.out.println(board[4] + " |" + board[5] + " |" + board[6]);
 		System.out.println("--------");
 		System.out.println(board[7] + " |" + board[8] + " |" + board[9]);
 	}
