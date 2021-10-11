281e945a06e8e54fa98f3dfc2662925999c30a9e
unknown <khomanesuraj8@gmail.com>
2021-10-11 12:27:25 +0530

    Make code indentation right

--- src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
+++ src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
@@ -12,29 +12,29 @@ public class TicTacToeGame {
         playerChoice();
    }
 	//method: create Board
-		public static void createBoard(){
-			board = new char[10];
-	        	for(int i = 1; i < board.length; i++){
-	                	board[i] = ' ';
-	        	}
-	     }
+	public static void createBoard(){
+		board = new char[10];
+	  	for(int i = 1; i < board.length; i++){
+	       	board[i] = ' ';
+	   	}
+	}
 		
-		//method: player choice	
-		public static void playerChoice() {
+	//method: player choice	
+	public static void playerChoice() {
 			
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter player choise 'X' or 'O'");
-			player = sc.next().charAt(0);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter player choise 'X' or 'O'");
+		player = sc.next().charAt(0);
 			
-			//check valid character
-			if(player != 'X' && player != 'O') {
-				System.out.println("You Enter invalid character, please enter 'X' or 'O' ");
-			}
-			else if(player == 'X') {	
-				computer = 'O';
-			}
-			else {
-				computer = 'X';	
-			}
+		//check valid character
+		if(player != 'X' && player != 'O') {
+			System.out.println("You Enter invalid character, please enter 'X' or 'O' ");
 		}
+		else if(player == 'X') {	
+			computer = 'O';
+		}
+		else {
+			computer = 'X';	
+		}
+	}
 }
