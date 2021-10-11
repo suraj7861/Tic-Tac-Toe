1e8f9831d9137cc89d8a6679516780597ebfd7ba
unknown <khomanesuraj8@gmail.com>
2021-10-11 12:20:24 +0530

    Make code indentation right

--- src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
+++ src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
@@ -13,39 +13,39 @@ public class TicTacToeGame {
         showBoard();
    }
 	//method: create Board
-		public static void createBoard(){
-			board = new char[10];
-	        	for(int i = 1; i < board.length; i++){
-	                	board[i] = ' ';
-	        	}
-	     }
+	public static void createBoard(){
+		board = new char[10];
+	       	for(int i = 1; i < board.length; i++){
+	               	board[i] = ' ';
+	       	}
+	    }
 		
-		//method: player choice	
-		public static void playerChoice() {
+	//method: player choice	
+	public static void playerChoice() {
 			
-			Scanner sc = new Scanner(System.in);
-			System.out.println("Enter player choise 'X' or 'O'");
-			player = sc.next().charAt(0);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter player choise 'X' or 'O'");
+		player = sc.next().charAt(0);
 			
-			//check valid character
-			if(player != 'X' && player != 'O') {
-				System.out.println("You Enter invalid character, please enter 'X' or 'O' ");
-			}
-			else if(player == 'X') {	
-				computer = 'O';
-			}
-			else {
-				computer = 'X';	
-			}
+		//check valid character
+		if(player != 'X' && player != 'O') {
+			System.out.println("You Enter invalid character, please enter 'X' or 'O' ");
 		}
+		else if(player == 'X') {	
+			computer = 'O';
+		}
+		else {
+			computer = 'X';	
+		}
+	}
 
-		//method show board
-		public static void showBoard() {
-			//print empty board 
-			System.out.println(board[1] + " |" + board[2] + " |" + board[3]);
-			System.out.println("--------");
-		    System.out.println(board[4] + " |" + board[5] + " |" + board[6]);
-			System.out.println("--------");
-			System.out.println(board[7] + " |" + board[8] + " |" + board[9]);
-		}
+	//method show board
+	public static void showBoard() {
+		//print empty board 
+		System.out.println(board[1] + " |" + board[2] + " |" + board[3]);
+		System.out.println("--------");
+        System.out.println(board[4] + " |" + board[5] + " |" + board[6]);
+		System.out.println("--------");
+		System.out.println(board[7] + " |" + board[8] + " |" + board[9]);
+	}
 }
