b23dd7da4093fbeabe6c29a0da84ecd82ec4d7ed
unknown <khomanesuraj8@gmail.com>
2021-10-11 08:40:04 +0530

    Added player choice

--- src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
+++ src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
@@ -1,10 +1,13 @@
 package com.Bridgelabz.TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 	public static char[] board;
 	public static void main(String[] args){
         System.out.println("play game");
         createBoard();
+        playerChoice();
    }
 	//creating method createBoard
 		public static void createBoard(){
@@ -13,4 +16,25 @@ public class TicTacToeGame {
 	                	board[i] = ' ';
 	        	}
 	     }
+		
+		public static void playerChoice() {
+			
+			char player1, player2;
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter player1 choise 'X' or 'O'");
+			player1 = sc.next().charAt('X');
+			System.out.println("Enter player1 choise 'X' or 'O'");
+			player2 = sc.next().charAt('O');
+			
+			if(player1 == 'X') {
+				player1 = 'X';
+				player2 = 'O';
+				
+			}
+			else if(player1 == 'O') {
+				player1 = 'X';
+				player2 = 'O';
+				
+			}
+		}
 }
