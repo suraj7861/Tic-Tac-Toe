67b95b221e4e8f6414cc8e0f09c6c6c326a6c8e9
unknown <khomanesuraj8@gmail.com>
2021-10-11 15:40:48 +0530

    check desired location free or not

--- src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
+++ src/com/Bridgelabz/TicTacToe/TicTacToeGame.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 
 public class TicTacToeGame {
 	//global variables
+	public static Scanner sc = new Scanner(System.in);
+	public static int location;
 	public static char[] board;
 	public static char player, computer;
 	
@@ -12,7 +14,8 @@ public class TicTacToeGame {
         playerChoice();
         showBoard();
         moveToDesireLocation();
-   }
+        }
+	
 	//method: create Board
 	public static void createBoard(){
 		board = new char[10];
@@ -24,7 +27,6 @@ public class TicTacToeGame {
 	//method: player choice	
 	public static void playerChoice() {
 			
-		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter player choise 'X' or 'O'");
 		player = sc.next().charAt(0);
 			
@@ -43,50 +45,56 @@ public class TicTacToeGame {
 	//method show board
 	public static void showBoard() {
 		//print empty board 
-		System.out.println(board[1] + " |" + board[2] + " |" + board[3]);
-		System.out.println("--------");
-		System.out.println(board[4] + " |" + board[5] + " |" + board[6]);
-		System.out.println("--------");
-		System.out.println(board[7] + " |" + board[8] + " |" + board[9]);
+		System.out.println(board[1] + " | " + board[2] + " | " + board[3]);
+		System.out.println("----------");
+		System.out.println(board[4] + " | " + board[5] + " | " + board[6]);
+		System.out.println("----------");
+		System.out.println(board[7] + " | " + board[8] + " | " + board[9]);
 	}
 	
 	//method: move to desired location
 	public static void moveToDesireLocation() {
 		System.out.println("Please enter desired location between 1 to 9");
-		Scanner sc = new Scanner(System.in);
-		int location = sc.nextInt();
-		//switch case to select location
-		switch(location) {
-		case 1:
-			board[1] = player;
-			break;
-		case 2:
-			board[2] = player;
-			break;
-		case 3:
-			board[3] = player;
-			break;
-		case 4:
-			board[4] = player;
-			break;
-		case 5:
-			board[5] = player;
-			break;
-		case 6:
-			board[6] = player;
-			break;
-		case 7:
-			board[7] = player;
-			break;
-		case 8:
-			board[8] = player;
-			break;
-		case 9:
-			board[9] = player;
-			break;
-		default:
-			System.out.println("Enter valid number");
+		location = sc.nextInt();
+		//check desired location is free or not
+		if(board[location] == ' ') {
+			//switch case to select location
+			switch(location) {
+			case 1:
+				board[1] = player;
+				break;
+			case 2:
+				board[2] = player;
+				break;
+			case 3:
+				board[3] = player;
+				break;
+			case 4:
+				board[4] = player;
+				break;
+			case 5:
+				board[5] = player;
+				break;
+			case 6:
+				board[6] = player;
+				break;
+			case 7:
+				board[7] = player;
+				break;
+			case 8:
+				board[8] = player;
+				break;
+			case 9:
+				board[9] = player;
+				break;
+			default:
+				System.out.println("Enter valid number");
+			}
 		}
+		else
+		{
+			System.out.println("this position is not Empty");
+		}
+		showBoard();
 	}
-
 }
